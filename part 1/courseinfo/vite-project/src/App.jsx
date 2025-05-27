c8ba021f6d51a6a2027731e45d8a992ec67c8740
Jeff Wang <wa.jeff@outlook.com>
2025-05-27 10:02:27 -0400

    Finished 1.1

---
+++ part 1/courseinfo/vite-project/src/App.jsx
@@ -0,0 +1,53 @@
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (
+      <h1>
+        {props.course}
+      </h1>
+    )
+  }
+
+  const Content = () =>{
+
+    return (
+      <section>
+        <p>
+          {part1} {exercises1}
+        </p>  
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      </section>
+    )
+  }
+  const Total = () =>{
+    return(
+      <>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </>
+      )
+    }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content/>
+      <Total/>
+    </div>
+  )
+}
+
+export default App
