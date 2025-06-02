145e45de083d1c761093097183baf0d0f90f1e25
Jeff Wang <wa.jeff@outlook.com>
2025-06-02 19:00:09 -0400

    Completed 1.3

--- part 1/courseinfo/vite-project/src/App.jsx
+++ part 1/courseinfo/vite-project/src/App.jsx
@@ -2,12 +2,18 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises:10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises:7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises:14
+  }
 
   const Header = (props) => {
     return (
@@ -27,16 +33,16 @@ const App = () => {
     }
     return (
       <section>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part part={part1.name}/>
+        <Part part={part2.name}/>
+        <Part part={part3.name}/>
       </section>
     )
   }
   const Total = () =>{
     return(
       <>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>Number of exercises {part1.exercises+part2.exercises+part3.exercises}</p>
       </>
       )
     }
