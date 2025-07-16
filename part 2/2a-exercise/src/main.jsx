31f6b846b2d9126cb7af5856f9ea1edb2c997a1c
Jeff Wang <wa.jeff@outlook.com>
2025-07-16 17:49:05 -0400

    Completed 2c reading

--- part 2/2a-exercise/src/main.jsx
+++ part 2/2a-exercise/src/main.jsx
@@ -1,5 +1,7 @@
-import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
-import App from './App'
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
