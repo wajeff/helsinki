ec9c81f493354b347f7f69a28c55042fcc5877ca
Jeff Wang <wa.jeff@outlook.com>
2025-07-16 17:54:14 -0400

    Completed 2c exercise

--- part 2/2b-exercise/src/App.jsx
+++ part 2/2b-exercise/src/App.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import { useEffect } from 'react'
+import axios from 'axios'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('add name')
   const [newNumber, setNewNumber] = useState('add number')
   const [newFilter, setNewFilter] = useState('')
-
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   const handleInputChange = (event) =>{
     const {name, value} = event.target
     if(name === "name"){
